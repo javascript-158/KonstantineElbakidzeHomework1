ab50f037ea6f3df3bda2fc08b510163fae36253b
konstantine elbakidze <koteelbakidze21@gmail.com>
2023-11-21 19:17:13 +0400

    finish

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let firstName=prompt("ჩაწერეთ თქვენი სახელი")
+let lastName=prompt("ჩაწერეთ თქვენი გვარი")
+let birthDate=prompt("ჩაწერეთ თქვენი დაბადების წელი")
+let address=prompt("ჩაწერეთ თქვენი საცხოვრებელი ადგილი")
+const presentYear=2023
+let yourAge=presentYear-birthDate
+alert("გამარჯობა, "+firstName+" "+lastName+". მიხარია, რომ უკვე "+yourAge+" წლის ხარ და "+address+"-ში ცხოვრობ!")
